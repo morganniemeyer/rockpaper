4b2c08883926b0d61d7db63580b093c182a4f804
Morgan Niemeyer <malineniemeyer@gmail.com>
2022-09-13 17:28:05 -0500

    begin player logic

--- app.js
+++ app.js
@@ -1,22 +1,42 @@
 /* Imports */
 
 /* State */
+let gameState = 'guess';
+let guess = '';
 let fight1 = document.getElementById('fight1');
 let fight2 = document.getElementById('fight2');
 let fight3 = document.getElementById('fight3');
+let fightPick = document.getElementById('fighter-select');
+let sbtxt = document.getElementById('subtext');
 /* Actions */
+function getRandomItem(array) {
+    const random = getRandomNumber(array.length);
+    const item = array[random];
+    return item;
+}
 function loadPage() {}
 
 /* Components */
 
+/*probability array*/
+const fighters = ['link', 'cucco', 'bokoblin'];
 /* Component */
 // get DOM
 // display
 // event listeners
 
-/*fight1.addEventListener('click', () => {
-  
-});*/
+fight1.addEventListener('click', () => {
+    fightPick.classList.add('hide');
+    sbtxt.classList.add('hide');
+});
+fight2.addEventListener('click', () => {
+    fightPick.classList.add('hide');
+    sbtxt.classList.add('hide');
+});
+fight3.addEventListener('click', () => {
+    fightPick.classList.add('hide');
+    sbtxt.classList.add('hide');
+});
 
 /* Run page load code */
 loadPage();
